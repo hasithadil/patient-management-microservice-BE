e4fff647ec1a8c85f15527bf39142929e790fb62
hasithadil <kah.dilshan@gmail.com>
2026-01-17 13:28:53 +0530

    initializing repo

---
+++ patient-service/src/main/java/com/pm/patientservice/model/Patient.java
@@ -0,0 +1,38 @@
+package com.pm.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Column(unique = true)
+    @Email
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    @Column(name = "registered_date")
+    private LocalDate registeredDate;
+}
